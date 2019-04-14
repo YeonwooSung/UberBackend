87d3620ae3cb6cbe77446a0ffa72f3d492172774
YeonwooSung <neos960518@gmail.com>
2019-04-15 05:43:21 +0900

    make a simple client program for stress test

--- testClient.js
+++ testClient.js
@@ -1,13 +1,68 @@
 var request = require('request');
 
-var options = {
-    uri: 'http://localhost:8080/login',
-    method: 'POST',
-    json: { subject: 'login', id: 'id', pw: 'pw' }
-};
+// sleep time expects milliseconds
+function sleep(time) {
+    return new Promise((resolve) => setTimeout(resolve, time));
+}
 
-request(options, function (error, response, body) {
-    if (!error && response.statusCode == 200) {
-        console.log(body) // Print the shortened url.
+async function loopRequest(num) {
+    console.log(num);
+
+    while (1) {
+        sleep(500).then(() => {
+            var options = {
+                uri: 'http://localhost:8080/login',
+                method: 'POST',
+                json: { subject: 'login', id: 'id', pw: 'pw' }
+            };
+
+            /*
+             * Send a POST request to use log in server
+             */
+            request(options, function (error, response, body) {
+                if (!error && response.statusCode == 200) {
+                    console.log(body) // Print the shortened url.
+                }
+            });
+        });
+
+        await sleep(500);
     }
-});
+}
+
+loopRequest(1);
+loopRequest(2);
+loopRequest(3);
+loopRequest(4);
+loopRequest(5);
+loopRequest(6);
+loopRequest(7);
+loopRequest(8);
+loopRequest(9);
+loopRequest(10);
+loopRequest(11);
+loopRequest(12);
+loopRequest(13);
+loopRequest(14);
+loopRequest(15);
+loopRequest(16);
+loopRequest(17);
+loopRequest(18);
+loopRequest(19);
+loopRequest(20);
+loopRequest(21);
+loopRequest(22);
+loopRequest(23);
+loopRequest(24);
+loopRequest(25);
+loopRequest(26);
+loopRequest(27);
+loopRequest(28);
+loopRequest(29);
+loopRequest(30);
+loopRequest(31);
+loopRequest(32);
+loopRequest(33);
+loopRequest(34);
+loopRequest(35);
+loopRequest(36);
